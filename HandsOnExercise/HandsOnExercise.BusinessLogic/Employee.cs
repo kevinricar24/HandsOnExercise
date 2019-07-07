fdb282dedab039a299826e2cec188dfd99c5dcee
kevin ricardo sejin <kevinricar24@hotmail.com>
2019-07-07 02:41:52 -0500

    Create Business Logic Layer

---
+++ HandsOnExercise/HandsOnExercise.BusinessLogic/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace HandsOnExercise.BusinessLogic
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string contractTypeName { get; set; }
+        public int roleId { get; set; }
+        public string roleName { get; set; }
+        public object roleDescription { get; set; }
+        public decimal hourlySalary { get; set; }
+        public decimal monthlySalary { get; set; }
+    }
+}
