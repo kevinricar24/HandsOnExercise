bc1623d49e7221921213a432bed55b9760a52b23
kevin ricardo sejin <kevinricar24@hotmail.com>
2019-07-07 02:53:16 -0500

    Integration factory with model

---
+++ HandsOnExercise/HandsOnExercise.BusinessLogic/DTOEmployee.cs
@@ -0,0 +1,31 @@
+﻿using HandsOnExercise.BusinessLogic.Factory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandsOnExercise.BusinessLogic
+{
+    public class DTOEmployee : Employee
+    {
+        public decimal salary
+        {
+            get
+            {
+                decimal response = 0;
+                if (!string.IsNullOrEmpty(contractTypeName))
+                {
+                    response = CalculationSalary(contractTypeName, hourlySalary, monthlySalary);
+                }
+                return response;
+            }
+        }
+
+        public decimal CalculationSalary(string paramContractTypeName, decimal paramHourlySalary, decimal paramMonthlySalary)
+        {
+            EmployeeSalary employeeSalary = new ConcreteEmployeeSalary();
+            ICalcSalary salary = employeeSalary.Salary(paramContractTypeName);
+            return salary.CalcSalary(paramHourlySalary, paramMonthlySalary);
+        }
+
+    }
+}
