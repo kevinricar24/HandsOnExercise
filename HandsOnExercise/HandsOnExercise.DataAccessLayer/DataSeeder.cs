444f002021a25858f9797253f814984ffa1e0cbc
kevin ricardo sejin <kevinricar24@hotmail.com>
2019-07-07 13:26:51 -0500

    Consume Web API and persist to Database

--- HandsOnExercise/HandsOnExercise.DataAccessLayer/DataSeeder.cs
+++ HandsOnExercise/HandsOnExercise.DataAccessLayer/DataSeeder.cs
@@ -1,6 +1,8 @@
 ﻿using HandsOnExercise.BusinessLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 
 namespace HandsOnExercise.DataAccessLayer
 {
@@ -11,11 +13,36 @@ namespace HandsOnExercise.DataAccessLayer
         {
             if (!context.Employees.Any())
             {
-                var employees = new List<DTOEmployee>()
-                                {
-                                new DTOEmployee { name = "kevin", contractTypeName="HourlySalaryEmployee", hourlySalary = 60000, monthlySalary = 80000  },
-                                new DTOEmployee { name = "dario", contractTypeName="MonthlySalaryEmployee", hourlySalary = 60000, monthlySalary = 80000  },
-                                };
+                List<DTOEmployee> employees = null;
+                //Dummy Data
+                //var employees = new List<DTOEmployee>()
+                //                {
+                //                new DTOEmployee { name = "kevin", contractTypeName="HourlySalaryEmployee", hourlySalary = 60000, monthlySalary = 80000  },
+                //                new DTOEmployee { name = "dario", contractTypeName="MonthlySalaryEmployee", hourlySalary = 60000, monthlySalary = 80000  },
+                //                };
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("http://masglobaltestapi.azurewebsites.net/api/");
+
+                    //HTTP GET
+                    var responseTask = client.GetAsync("Employees");
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<List<DTOEmployee>>();
+                        readTask.Wait();
+
+                        employees = readTask.Result;
+                    }
+                    else
+                    {
+                        employees = new List<DTOEmployee>();
+                    }
+                }
+
                 context.Employees.AddRange(employees);
                 context.SaveChanges();
             }
--- HandsOnExercise/HandsOnExercise.WebNetCore/Program.cs
+++ HandsOnExercise/HandsOnExercise.WebNetCore/Program.cs
@@ -19,9 +19,9 @@ namespace HandsOnExercise.WebNetCore
                     var context = services.GetRequiredService<ApplicationDbContext>();
                     DataSeeder.Initialize(context);
                 }
-                catch (Exception)
+                catch (Exception ex)
                 {
-                    Console.WriteLine("An error occurred while seeding the database.");
+                    Console.WriteLine("An error occurred while seeding the database: " + ex.Message);
                 }
             }
             host.Run();
